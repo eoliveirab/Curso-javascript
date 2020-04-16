d4d57e3c39d5aa2c59e953ef2daa0cdb36243445
Everton basilio de Oliveira <evinhoww@gmail.com>
2020-04-16 00:08:47 -0300

    Node.js  e javascripit
    Material Node.js
    Material javascripit

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60){ //condição simples - não tem else
+    console.log('Você ultrapassou a velocidade permitida!')
+}
+console.log('Dirija sempre usando cinto de segurança! ')
